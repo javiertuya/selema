64d3315d604019846b338e5887df523cb0019fcb
Javier <10879637+javiertuya@users.noreply.github.com>
2025-03-12 11:20:21 +0100

    Update visual-assert and portable dependencies

--- net/sharpen-docker/entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-echo "Working dir: $1"
-echo "Project dir: $2"
-echo "Sharpen args: $3"
-echo "Running sharpen..."
-cd "$1" && java -jar /sharpen/sharpencore-0.0.1-SNAPSHOT.jar $2 $3
--- net/sharpen-docker/sharpen-run.bat
+++
@@ -1,2 +0,0 @@
-rem run from solution folder
-docker build -t sharpen .\sharpen-docker && docker run -v %CD%:/sharpen/workdir sharpen . sharpen-temp/java @sharpen-all-options.txt
